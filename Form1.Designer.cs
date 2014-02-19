d09dcd89f8530086fc564b172491a672a0117812
Veysel Şahin <thevyxx@gmail.com>
2014-02-19 08:28:50 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace dersSecimKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtSure = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mynotifyicon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtKullanici = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(501, 61);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(75, 23);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1041, 448);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // txtSure
+            // 
+            this.txtSure.Location = new System.Drawing.Point(116, 28);
+            this.txtSure.Name = "txtSure";
+            this.txtSure.Size = new System.Drawing.Size(100, 20);
+            this.txtSure.TabIndex = 2;
+            this.txtSure.Text = "20";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kontrol Süresi(sn) :";
+            // 
+            // mynotifyicon
+            // 
+            this.mynotifyicon.BalloonTipText = "Şuan buradayım ben bakıyorum sen raad ol.";
+            this.mynotifyicon.BalloonTipTitle = "Başgan Çalışıyo";
+            this.mynotifyicon.Icon = ((System.Drawing.Icon)(resources.GetObject("mynotifyicon.Icon")));
+            this.mynotifyicon.Text = "notifyIcon1";
+            this.mynotifyicon.Visible = true;
+            this.mynotifyicon.DoubleClick += new System.EventHandler(this.mynotifyicon_DoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.linkLabel1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtSifre);
+            this.groupBox1.Controls.Add(this.txtKullanici);
+            this.groupBox1.Controls.Add(this.btnBasla);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtSure);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 348);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1041, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtKullanici
+            // 
+            this.txtKullanici.Location = new System.Drawing.Point(310, 28);
+            this.txtKullanici.Name = "txtKullanici";
+            this.txtKullanici.Size = new System.Drawing.Size(162, 20);
+            this.txtKullanici.TabIndex = 4;
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(310, 63);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(162, 20);
+            this.txtSifre.TabIndex = 5;
+            this.txtSifre.UseSystemPasswordChar = true;
+            this.txtSifre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSifre_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kullanıcı Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Şifre :";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(788, 28);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 448);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Ders Kontrol Şeysi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txtSure;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon mynotifyicon;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKullanici;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Runtime.InteropServices;
+namespace dersSecimKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\drs"))
+            {
+                if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\drs\\conf.ini"))
+                {
+                    string [] satirlar = File.ReadAllLines(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\drs\\conf.ini");
+                    if (satirlar!=null&&Microsoft.VisualBasic.Information.IsDate( satirlar[0].Split(':')[1])&&satirlar[0].Split(':')[1]==DateTime.Now.ToString("yyyy-MM-dd"))
+                    {
+                        MessageBox.Show("Sistemi meşgul etme kardeşim bugün senin ders seçim günün değilmiş.Bugün git yarın gel.","Hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\drs\\conf.ini");
+                    }
+                }
+
+            }
+            DisableClickSounds();
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtKullanici.Text)||string.IsNullOrEmpty(txtSifre.Text))
+            {
+                MessageBox.Show("Kullanıcı adı veya şifre boş geçilemez.");
+                return;
+            }
+
+           
+
+            if (Microsoft.VisualBasic.Information.IsNumeric(txtSure.Text))
+            {
+                if (Convert.ToInt32(txtSure.Text) < 20)
+                {
+                    MessageBox.Show("20 saniyeden aşağısı kurtarmaz.");
+                }
+                else
+                {
+                    txtSure.Text = "20";
+                    timer1.Interval = Convert.ToInt32(txtSure.Text) * 1000;
+                    timer1.Start();
+                }
+            }
+            else
+            {
+                timer1.Interval = 20000;
+                timer1.Start();
+            }
+            MessageBox.Show("Şimdi hiçbir şeye tıklamadan arkanıza yaslanın ve sistemin açıldığına dair uyarıyı bekleyin.","Birşeye tıklamanıza gerek yok",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            submitted = false;
+            webBrowser1.Navigate("http://orgunkayit.anadolu.edu.tr/kayit/home.seam");
+        }
+        int tickCount = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tickCount++;
+            if (!webBrowser1.IsBusy && tickCount==1)
+            {
+                webBrowser1.Navigate("http://orgunkayit.anadolu.edu.tr/kayit/home.seam");
+                submitted = false;
+            }
+           
+          
+            if (tickCount>2&&tickCount%2==0)
+            {
+               mynotifyicon.ShowBalloonTip(10000, "Ders kaydı başlayacak", "Ders kaydı başlayacak galiba şuan sisteme giriş yapılamıyor gibi görünüyor.Sistem "+tickCount*Convert.ToInt32(txtSure.Text)+" saniyedir cevap vermiyor.", ToolTipIcon.Info);
+            }
+
+        }
+        bool submitted = false;
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tickCount = 0;
+
+            if (!submitted)
+            {
+
+                HtmlDocument doc = webBrowser1.Document;
+                doc.GetElementById("loginForm:username").InnerText = txtKullanici.Text;
+                doc.GetElementById("loginForm:password").InnerText = txtSifre.Text;
+                doc.GetElementById("loginForm:submit").InvokeMember("click");
+                
+                submitted = true;
+                
+            }
+            else
+            {
+
+
+
+                if (webBrowser1.Url.LocalPath.Contains("kayit.seam"))
+                {
+                    mynotifyicon.ShowBalloonTip(10000, "Ders kaydı başladı", "Ders kaydı başladı koşşş", ToolTipIcon.Info);
+                    this.Show();
+                    this.WindowState = FormWindowState.Maximized;
+                    this.Activate();
+                    MessageBox.Show("Ders kaydı başladı.","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+                if (webBrowser1.Url.OriginalString.Contains("action=5"))
+                {
+                       mynotifyicon.ShowBalloonTip(10000, "Bir sorunumuz var", "Hocam sen hangi gün ders seçimi yapacağını bildiğinden emin misin?", ToolTipIcon.Info);
+                       if (!Directory.Exists(Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData)+ "\\drs"))
+                       {
+                           Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\drs");
+
+                       }
+                      File.AppendAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\drs\\conf.ini","NotAllowedAt:"+DateTime.Now.ToString("yyyy-MM-dd"));
+                      this.Show();
+                      this.WindowState = FormWindowState.Maximized;
+                      this.Activate();
+                      MessageBox.Show("Ders seçimizi bugün değilmiş gibi görünüyor.Lütfen tarihten emin olunuz.","Hata",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                      Application.Exit();
+                }
+            }
+        }
+
+        private void btnKucult_Click(object sender, EventArgs e)
+        {
+
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                mynotifyicon.Visible = true;
+                mynotifyicon.ShowBalloonTip(500);
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                mynotifyicon.Visible = false;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                mynotifyicon.Visible = true;
+                
+                mynotifyicon.ShowBalloonTip(500);
+                this.Hide();
+               
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                mynotifyicon.Visible = false;
+            }
+        }
+
+        private void mynotifyicon_DoubleClick(object sender, EventArgs e)
+        {
+            
+            this.Show();
+            this.WindowState = FormWindowState.Maximized;
+            this.Activate();
+        }
+
+        private void txtSifre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnBasla_Click(sender, e);
+            }
+        }
+
+        const int FEATURE_DISABLE_NAVIGATION_SOUNDS = 21;
+        const int SET_FEATURE_ON_PROCESS = 0x00000002;
+
+        [DllImport("urlmon.dll")]
+        [PreserveSig]
+        [return: MarshalAs(UnmanagedType.Error)]
+        static extern int CoInternetSetFeatureEnabled(
+            int FeatureEntry,
+            [MarshalAs(UnmanagedType.U4)] int dwFlags,
+            bool fEnable);
+
+        static void DisableClickSounds()
+        {
+            CoInternetSetFeatureEnabled(
+                FEATURE_DISABLE_NAVIGATION_SOUNDS,
+                SET_FEATURE_ON_PROCESS,
+                true);
+        }
+
+      
+
+       
+    }
+
+
+}
+
